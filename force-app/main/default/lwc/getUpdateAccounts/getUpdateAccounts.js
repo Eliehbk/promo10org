c18ed87476577dcf88cb7d6c63037f8c915d8f93
Elie Habka <Elie.Habka@EITECH.LOCAL>
2026-06-29 12:37:06 +0300

    filter accounts and update the description2

--- force-app/main/default/lwc/getUpdateAccounts/getUpdateAccounts.js
+++ force-app/main/default/lwc/getUpdateAccounts/getUpdateAccounts.js
@@ -1,8 +1,8 @@
 import { LightningElement ,track} from 'lwc';
+import { NavigationMixin } from "lightning/navigation";
 import getAccounts from '@salesforce/apex/accountActionController.getAccounts';
 import updateDescription from '@salesforce/apex/accountActionController.updateDescription';
-
-export default class GetUpdateAccounts extends LightningElement {
+export default class GetUpdateAccounts extends NavigationMixin(LightningElement) {
  @track accounts =[];
  @track selectedAccount=null;
 newDesc ='';
@@ -16,6 +16,20 @@ catch(error){
 console.error(error);
 }
 }
+handleNavigateToRecord(event) {
+    console.log('hellllllllllllllll');
+        const accId = event.target.dataset.id;
+        console.log(accId);
+        this[NavigationMixin.Navigate]({
+            type: 'standard__recordPage',
+            attributes: {
+                recordId: accId,
+                objectApiName: 'Account',
+                actionName: 'view'
+            }
+        });
+    }
+
 handleSelect(event){
 const accId=event.target.dataset.id;
 this.selectedAccount=this.accounts.find(acc=> acc.Id===accId)
